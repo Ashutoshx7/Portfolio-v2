a3e6cfbd4af56d4da2395389d82c690564559da4
Piyushrathoree <meonpath008@gmail.com>
2025-11-20 04:38:24 +0530

    Add new SVG icons for various technologies including Git, JavaScript, Python, React, TypeScript, and Tailwind CSS
    - Added gitmerge.svg and gitopen.svg for Git icons
    - Introduced go-svgrepo-com.svg for Go language
    - Included js.svg for JavaScript
    - Added nextdark.svg and nextlight.svg for Next.js
    - Introduced node.svg for Node.js
    - Added prisma.svg for Prisma
    - Included python.svg for Python
    - Added react.svg for React
    - Introduced tailwind.svg for Tailwind CSS
    - Added ts.svg for TypeScript

---
+++ app/components/Tech.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+
+interface TechProps {
+  logo: string;
+  name: string;
+}
+const Tech = (props: TechProps) => {
+  return (
+    <div className="flex gap-1 px-3 py-1.5 border border-dashed bg-neutral-800 items-center justify-center ">
+      <img src={props.logo} alt={props.name} className="h-5 w-5 " />
+      <p className="text-center text-sm font-semibold">{props.name}</p>
+    </div>
+  );
+};
+
+export default Tech;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Avatar from "./components/Avatar";
 
 import Graph from "./components/Graph";
+import Tech from "./components/Tech";
 
 
 export default function Home() {
@@ -8,7 +9,7 @@ export default function Home() {
     <div className="flex flex-col justify-center items-center min-h-screen py-2 bg-neutral-950">
     <Graph/>
     <Avatar/>
-
+    <Tech logo="/icons/vercel.svg" name="Vercel"/>
     </div>
   );
 }
