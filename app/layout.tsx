27f0ad732dc8406efdb5646074abbe466c176174
Piyushrathoree <meonpath008@gmail.com>
2025-11-21 21:37:31 +0530

    feat: import Navbar component into RootLayout and remove ThemeProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono ,Instrument_Serif} from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
+import { Navbar } from "@/components/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -40,6 +40,7 @@ export default function RootLayout({
           disableTransitionOnChange
           
         > */}
+        <Navbar />
           {children}
         {/* </ThemeProvider> */}
       </body>
