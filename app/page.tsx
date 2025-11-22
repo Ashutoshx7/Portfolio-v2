8aa0ea8974d1ec7b1b4c1ef46779c1959059dc83
Piyushrathoree <meonpath008@gmail.com>
2025-11-23 02:21:46 +0530

    refactor: remove SmoothCursor component and clean up Home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,9 @@ import Avatar from "@/components/Avatar";
 import { Card } from "@/components/Card";
 import Image from "next/image";
 import Graph from "@/components/Graph";
-import Tech from "@/components/Tech";
-import ThemeToggle from "@/components/theme-toggle";
+
 import Hero from "@/components/Hero";
-import { SmoothCursor } from "@/components/ui/smooth-cursor";
+
 import Projects from "@/components/Projects";
 
 export default function Home() {
@@ -13,7 +12,7 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-start items-center min-h-screen py-2 mt-30">
       <Hero />
-      <SmoothCursor />
+     
       <Projects />  
       <Graph />
     </div>
