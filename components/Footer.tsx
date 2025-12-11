d785af0c148ef4b9d40a6e5cc1947d6f99929bec
Piyushrathoree <meonpath008@gmail.com>
2025-12-11 19:08:34 +0530

    just a minor change

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,8 +7,10 @@ const Footer = () => {
     <footer className="sm:w-230 mx-auto w-screen overflow-x-hidden bg-transparent  border-zinc-200 dark:border-white/10 py-6 px-4 md:px-8 mt-10">
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center gap-4 text-sm text-zinc-600 dark:text-zinc-400">
         <div className="flex items-center gap-1">
-          <span>Made by Piyush with</span>
-          <LucideHeart className="w-4 h-4 text-red-500 " />
+          <span className="flex items-center gap-1">
+            Made with <LucideHeart className="w-4 h-4 text-red-500 " />
+            by Piyush
+          </span>
         </div>
 
         <Link
