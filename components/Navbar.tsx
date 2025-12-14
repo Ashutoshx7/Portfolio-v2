4ed230fa436142186e419a661bb6716d68476989
Piyushrathoree <meonpath008@gmail.com>
2025-12-14 19:28:48 +0530

    feat: Implement a new Navbar component with theme toggling, a layout container, and new icon dependencies.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,8 @@ import Link from "next/link";
 import Container from "@/components/containers";
 import { motion, useScroll, useMotionValueEvent } from "framer-motion";
 import ThemeToggle from "./theme-toggle";
-import { Home } from "lucide-react";
+import { Home12Icon } from "@hugeicons/core-free-icons/index";
+import { HugeiconsIcon } from "@hugeicons/react";
 
 const Navbar = () => {
   const navItems = [
@@ -48,21 +49,24 @@ const Navbar = () => {
           duration: 0.3,
           ease: "easeOut",
         }}
-        className="fixed inset-x-0 top-0 z-50 flex sm:max-w-230 mx-auto items-center justify-between 
+        className="fixed inset-x-0 top-0 z-50 sm:rounded-md flex sm:max-w-230 mx-auto items-center justify-between 
         px-4 py-3 bg-neutral-50/50 backdrop-blur-sm dark:bg-neutral-950/70  font-black  text-neutral-900 dark:text-neutral-50 transition-all duration-300 backdrop-filter border  border-dashed border-neutral-200 dark:border-neutral-700/50 "
       >
         <Link
           href="/"
           className=" flex items-center justify-center dark:bg-neutral-800/60  "
         >
-          <Home className="dark:text-neutral-300 text-neutral-500 dark:hover:text-neutral-50 hover:text-neutral-950 duration-200" />
+          <HugeiconsIcon
+            icon={Home12Icon}
+            className="dark:text-neutral-300 text-neutral-500 dark:hover:text-neutral-50 hover:text-neutral-950 duration-200"
+          />
         </Link>
 
         {/* Navigation links on the right */}
         <div className="ml-auto flex flex-wrap items-center justify-end gap-3 sm:gap-6">
           {navItems.map((item, idx) => (
             <Link
-              className="text-sm relative px-3 py-1.5 text-neutral-600 dark:text-neutral-300 font-medium transition-colors hover:text-neutral-900"
+              className="text-sm relative px-3 py-1.5 text-neutral-500 dark:text-neutral-300 font-medium transition-colors hover:text-neutral-900"
               href={item.href}
               key={idx}
               onMouseEnter={() => setHovered(idx)}
